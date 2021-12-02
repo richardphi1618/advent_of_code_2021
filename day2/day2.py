c5b26027c2539855aeb492f91a630750ce87ec2e
richardphi1618 <richardphi1618@gmail.com>
2021-12-02 08:19:20 -0500

    day 2 part 2

--- day2/day2.py
+++ day2/day2.py
@@ -7,14 +7,11 @@ def load_file(filename:str) -> list[str]:
 
     return data
 
-
-if __name__ == '__main__':
-    data = load_file('day2/sampleset.txt')
-    
+def part1(input:list[str]) -> tuple[int, int]: 
+    #Part 1
     depth = 0
     horizontal_position = 0
-
-    for i in data:
+    for i in input:
         command = i.split(" ")
         if 'up' in i:
             depth -= int(command[1])
@@ -22,8 +19,43 @@ if __name__ == '__main__':
             depth += int(command[1])
         elif 'forward' in i:
             horizontal_position += int(command[1])
-    
+
     print (depth)
     print (horizontal_position)
 
     print (f'answer: {depth*horizontal_position}')
+
+    return depth, horizontal_position
+
+def part2(input:list[str]) -> tuple[int, int, int]: 
+    #Part 1
+    aim = 0
+    depth = 0
+    horizontal_position = 0
+    for i in input:
+        command = i.split(" ")
+        if 'up' in i:
+            aim -= int(command[1])
+        elif 'down' in i:
+            aim += int(command[1])
+        elif 'forward' in i:
+            horizontal_position += int(command[1])
+            depth += aim * int(command[1])
+    
+    print (aim)
+    print (depth)
+    print (horizontal_position)
+
+    print (f'answer: {depth*horizontal_position}')
+
+    return aim, depth, horizontal_position
+
+
+if __name__ == '__main__':
+    data = load_file('day2/sampleset.txt')
+
+    print("---------- Part 1 ----------")
+    part1(data)
+    print("---------- Part 2 ----------")
+    part2(data)
+    
