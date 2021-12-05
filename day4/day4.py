c32cd8faf5af7b098676d83fbfd3afded61259f9
Richard Blanchette <richardphi1618@gmail.com>
2021-12-04 19:17:38 -0500

    day4 refactored as class

--- day4/day4.py
+++ day4/day4.py
@@ -1,37 +1,63 @@
 import copy
 
-def load_file(filename:str) -> dict:
-    data = {}
+class Bingo_Game:
+    """Represents the board that the game of tic-tac-toe is played on."""
 
+    raw_data: list[str]
+    number_drawings: list[int]
+    num_of_boards: int
+    boards_str: list[list[str]]
+    boards_int: list[list[list[int]]]
+    boards_matches: list[list[list[bool]]]
+
+    def __init__(self) -> None:
+        self.raw_data = []
+        self.number_drawings = []
+        self.num_of_boards = 0
+        self.boards_str = [[]]
+        self.boards_int = [[[]]] #[board_num][row][column]
+        self.boards_matches = [[[]]]
+
+    def build_game(self, filename:str) -> None:
+        
+        self.raw_data = load_file(filename)
+
+        self.number_drawings = list(map(int, self.raw_data[0].split(",")))  
+        self.num_of_boards = int((len(self.raw_data)-1)/6)
+
+        self.boards_str = [[]]*self.num_of_boards
+        board_num = 0
+
+        for i in self.raw_data[2::]:
+            if i != '':
+                self.boards_str[board_num] += [i]
+            else:
+                board_num += 1
+
+        self.boards_int = [[[]]*5]*self.num_of_boards
+
+        for idx,x in enumerate(self.boards_int):
+            for idy,y in enumerate(x):
+                self.boards_int[idx][idy] = self.boards_str[idx][idy].split(" ") 
+                while '' in self.boards_int[idx][idy] : self.boards_int[idx][idy].remove('')
+                self.boards_int[idx][idy] = list(map(int, self.boards_int[idx][idy])) 
+
+        self.boards_matches = [[[False]*5]*5]*self.num_of_boards
+
+
+def load_file(filename:str) -> list:
     my_file = open(filename, 'r')
     content = my_file.read()
-    data['raw_data'] = content.split("\n")
-    my_file.close()
+    output = content.split("\n")
+    my_file.close()  
 
-    data['number_drawings'] = list(map(int, data['raw_data'][0].split(",")))  
-    data['num_of_boards'] = int((len(data['raw_data'])-1)/6)
-
-    data['boards_str'] = [[]]*data['num_of_boards']
-    board_num = 0
-
-    for i in data['raw_data'][2::]:
-        if i != '':
-            data['boards_str'][board_num] += [i]
-        else:
-            board_num += 1
-
-    data['boards_int'] = [[[]]*5]*data['num_of_boards']
-
-    for idx,x in enumerate(data['boards_int']):
-        for idy,y in enumerate(x):
-            data['boards_int'][idx][idy] = data['boards_str'][idx][idy].split(" ") 
-            while '' in data['boards_int'][idx][idy] : data['boards_int'][idx][idy].remove('')
-            data['boards_int'][idx][idy] = list(map(int, data['boards_int'][idx][idy]))  
-
-    return data
+    return output
 
 def part1 (input:dict) -> None:
-    print(input)
+    #print(input)
+    part1 = Bingo_Game()
+    part1.build_game('day4/sampleset_ex.txt')
+    print(part1)
     return None
 
 if __name__ == '__main__':
