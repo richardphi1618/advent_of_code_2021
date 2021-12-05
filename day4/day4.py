0aa397de68c6f9dbba9f308f4e91c6c18a1d6a50
richardphi1618 <richardphi1618@gmail.com>
2021-12-04 21:53:32 -0500

    day4 part 1 done

--- day4/day4.py
+++ day4/day4.py
@@ -9,14 +9,18 @@ class Bingo_Game:
     boards_str: list[list[str]]
     boards_int: list[list[list[int]]]
     boards_matches: list[list[list[bool]]]
+    game_over : bool
+    turn : int
+    winning_board : int
 
     def __init__(self) -> None:
         self.raw_data = []
         self.number_drawings = []
         self.num_of_boards = 0
-        self.boards_str = [[]]
-        self.boards_int = [[[]]] #[board_num][row][column]
+        self.boards_str = []
         self.boards_matches = [[[]]]
+        self.game_over = False
+        self.turn = 0
 
     def build_game(self, filename:str) -> None:
         
@@ -25,32 +29,80 @@ class Bingo_Game:
         self.number_drawings = list(map(int, self.raw_data[0].split(",")))  
         self.num_of_boards = int((len(self.raw_data)-1)/6)
 
-        self.boards_str = [[]]*self.num_of_boards
-        board_num = 0
+        self.boards_str = []
+        
 
         for i in self.raw_data[2::]:
             if i != '':
-                self.boards_str[board_num] += [i]
-            else:
-                board_num += 1
+                self.boards_str += [i]
 
-        self.boards_int = [[[]]*5]*self.num_of_boards
+        self.boards_int = [[[-1 for col in range(5)] for row in range(5)] for board in range(self.num_of_boards)]
 
-        for idx,x in enumerate(self.boards_int):
-            for idy,y in enumerate(x):
-                self.boards_int[idx][idy] = self.boards_str[idx][idy].split(" ") 
-                while '' in self.boards_int[idx][idy] : self.boards_int[idx][idy].remove('')
-                self.boards_int[idx][idy] = list(map(int, self.boards_int[idx][idy])) 
+        row_num = 0
 
-        self.boards_matches = [[[False]*5]*5]*self.num_of_boards
+        for board_num in range(0,self.num_of_boards):
+            for board_row in range(0,5):
+                row_of_interest = self.boards_str[row_num].split(" ")
+                while '' in row_of_interest : row_of_interest.remove('')
+                row_of_interest = list(map(int, row_of_interest))
+                self.boards_int[board_num][board_row] = copy.deepcopy(row_of_interest)
+                row_num += 1
 
-    def matches (self) -> None:
-        self.
+        self.boards_matches = [[[False for col in range(5)] for row in range(5)] for board in range(self.num_of_boards)]
+
+
+    def find_matches (self, drawn_number: int) -> None:
+        print(f"This round drawn number:{drawn_number} ")
+        for board_dx, board in enumerate(self.boards_int):
+            for row_dx, row in enumerate(board):
+                for column_dx, item in enumerate(row): #item can double as column
+                    if drawn_number == item:
+                        self.boards_matches[board_dx][row_dx][column_dx] = True        
         return None
     
-    def check_for_winner (self) -> int:
-        pass
-        return 0
+    def check_for_winner (self, verbose = True) -> None:
+        self.turn += 1
+
+        #check rows
+        for board_dx, board in enumerate(self.boards_matches):
+            for row_dx, row in enumerate(board):
+                if all(self.boards_matches[board_dx][row_dx]):
+                    self.winning_board = board_dx + 1
+                    self.winning_row = self.boards_int[board_dx][row_dx]
+                    self.game_over = True
+        
+        for board_dx, board in enumerate(self.boards_matches):
+            for col in range(0,5):
+                column_of_interest_matches = [board[0][col], board[1][col], board [2][col], board [3][col], board [4][col]]
+                if all(column_of_interest_matches):
+                    self.winning_board = board_dx + 1
+                    column_of_interest = []
+                    
+                    for i in self.boards_int[board_dx]:
+                        column_of_interest += [i[col]]
+
+                    self.winning_row = column_of_interest
+                    self.game_over = True
+
+        if verbose:
+            if self.game_over:
+                print(f"Game Over!!! \nTurn Num. {self.turn}\nWinning is board:{self.winning_board} \nWinning row: {self.winning_row}")
+            else:
+                print(f"Game continues... \n Turn Num. {self.turn}")
+        
+        return None
+        
+    def sum_unmatched(self) -> int:
+        val = 0
+
+        winning_board = self.boards_int[self.winning_board-1]
+        
+        for row_dx, row in enumerate(winning_board):
+            for col_dx, item in enumerate(row):
+                if self.boards_matches[self.winning_board-1][row_dx][col_dx] == False:
+                    val += item
+
+        return val
 
 
 def load_file(filename:str) -> list:
@@ -64,13 +116,19 @@ def load_file(filename:str) -> list:
 def part1 (input:dict) -> None:
     #print(input)
     part1 = Bingo_Game()
-    part1.build_game('day4/sampleset_ex.txt')
-    print(part1)
+    part1.build_game('day4/sampleset.txt')
 
     for i in part1.number_drawings:
-        part1.matches()
+        part1.find_matches(i)
         part1.check_for_winner()
+        if part1.game_over: break
     
+    unmatached_sum = part1.sum_unmatched()
+
+    print(f"unmatched sum: {unmatached_sum}")
+    print(f"winning drawing: {part1.number_drawings[part1.turn-1]}")
+
+    print(f"\nanswer: {unmatached_sum*part1.number_drawings[part1.turn-1]}")
     return None
 
 if __name__ == '__main__':
